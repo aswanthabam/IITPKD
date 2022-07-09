a076e04aca531e2ddf9ca7a73b4778db939d9ae7
avc-tech <avctech.official@gmail.com>
2022-07-09 21:17:18 +0530

    hhh

---
+++ script.js
@@ -0,0 +1,56 @@
+var slideIndex = 0;
+
+function setUp()
+{
+	mar = document.querySelector(".marquee-top");
+	topbar = document.querySelector(".topbar");
+	topbar_banner = document.querySelector(".topbar .icon img");
+	parallex = document.querySelector(".parallex");
+	main = document.querySelector(".main");
+	
+	mar.onmouseover = function(e)
+	{
+		mar.stop();
+	}
+	mar.onmouseout = function(e)
+	{
+		mar.start();
+	}
+/*	window.onscroll = function()
+	{
+		if(window.scrollY > parallex.getBoundingClientRect().height -100)
+		{
+			main.style.borderRadius = "0px";
+			topbar.style.padding = "2px";
+			topbar.style.width = "calc(100% - 4px)"
+			//topbar_banner.classList.add("scrolled");
+		}
+		else{
+			main.style.borderRadius = "30px 30px 0px 0px";
+			topbar.style.padding = "5px";
+			topbar.style.width = "calc(100% - 10px)"
+			//topbar_banner.classList.remove("scrolled");
+		}
+	}*/
+}
+
+function carousel(elem) {
+  var i;
+  var x = elem.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+  	/*x[i].style.transition = "all 1s ease";
+    x[i].style.opacity = 0;*/
+    x[i].style.display = "none";
+  }
+  setTimeout(function(x){
+  	for (i = 0; i < x.length; i++) {
+  		
+    }
+  },1000,x);
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  /*x[slideIndex-1].style.transition = "all 1s ease";
+  x[slideIndex-1].style.opacity = 1;*/
+  setTimeout(carousel, 3000,elem); // Change image every 2 seconds
+}
