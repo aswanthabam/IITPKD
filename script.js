67eb5003df4e85aa6850eccdf9257596fe1d9fea
aswanthabam <aswanth.abam@gmail.com>
2022-07-24 13:21:41 +0530

    gg

--- script.js
+++ script.js
@@ -81,26 +81,27 @@ function setUp()
 	    glowInText.append(span);
 	  });
 	});
-	const phrases = [
-	  'you\'re going to realize',
-	  'just as I did',
-	  'that there\'s a difference',
-	  'between knowing the path',
-	  'and walking the path'
-	]
+	
+	// const phrases = [
+	//   'you\'re going to realize',
+	//   'just as I did',
+	//   'that there\'s a difference',
+	//   'between knowing the path',
+	//   'and walking the path'
+	// ]
 
-	const el = document.querySelector('.text')
-	const fx = new TextScramble(el)
+	// const el = document.querySelector('.text')
+	// const fx = new TextScramble(el)
 
-	let counter = 0
-	const next = () => {
-	  fx.setText(phrases[counter]).then(() => {
-	    setTimeout(next, 800)
-	  })
-	  counter = (counter + 1) % phrases.length
-	}
+	// let counter = 0
+	// const next = () => {
+	//   fx.setText(phrases[counter]).then(() => {
+	//     setTimeout(next, 800)
+	//   })
+	//   counter = (counter + 1) % phrases.length
+	// }
 
-	next()
+	// next()
 //	mar = document.querySelector(".marquee-top");
 	topbar = document.querySelector(".topbar");
 	topbar_banner = document.querySelector(".topbar .icon img");
